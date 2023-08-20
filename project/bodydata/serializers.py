c570a81997508c4738d99a6b3a0ce4ebc7dfc8ef
1844ryudai@gmail.com <1844ryudai>
2023-08-20 14:56:22 +0900

    feat: Minor modification

--- project/bodydata/serializers.py
+++ project/bodydata/serializers.py
@@ -1,3 +1,5 @@
+from decimal import ROUND_HALF_UP, Decimal
+
 from rest_framework import serializers
 
 from .models import BodyData
@@ -22,16 +24,24 @@ class WeightCalculationSerializer(serializers.Serializer):
     cinderella_weight = serializers.SerializerMethodField()
 
     def height_squared(self, height: float) -> float:
+        """身長をcm→mに変換し2乗する"""
         return (height / 100) ** 2
 
+    def round_up_and_down(self, target):
+        """四捨五入メソッド"""
+        exp = Decimal("0.1")
+        return str(Decimal(target).quantize(exp, ROUND_HALF_UP))
+
     def get_bmi(self, obj):
-        return obj["weight"] / self.height_squared(obj["height"])
+        return self.round_up_and_down(
+            obj["weight"] / self.height_squared(obj["height"])
+        )
 
     def get_appropriate_body_weight(self, obj):
-        return self.height_squared(obj["height"]) * 22
+        return self.round_up_and_down(self.height_squared(obj["height"]) * 22)
 
     def get_beauty_weight(self, obj):
-        return self.height_squared(obj["height"]) * 20
+        return self.round_up_and_down(self.height_squared(obj["height"]) * 20)
 
     def get_cinderella_weight(self, obj):
-        return self.height_squared(obj["height"]) * 18
+        return self.round_up_and_down(self.height_squared(obj["height"]) * 18)
