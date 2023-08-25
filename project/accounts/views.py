2c46143b950b4be76ac456a19eee1345b82510e9
1844ryudai@gmail.com <1844ryudai>
2023-08-26 08:57:26 +0900

    feat: Add mypage api

--- project/accounts/views.py
+++ project/accounts/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status, views
+from rest_framework.response import Response
 
 from .models import CustomUser
 from .serializers import AccountSerializer
@@ -12,3 +13,11 @@ class AccountRetrieveAPIView(generics.RetrieveAPIView):
     queryset = CustomUser.objects.all().prefetch_related("bodydata_set")
     serializer_class = AccountSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+class AccountMypageAPIView(views.APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        serializer = AccountSerializer(request.user)
+        return Response(serializer.data, status.HTTP_200_OK)
--- project/apis/urls.py
+++ project/apis/urls.py
@@ -1,4 +1,8 @@
-from accounts.views import AccountCreateAPIView, AccountRetrieveAPIView
+from accounts.views import (
+    AccountCreateAPIView,
+    AccountMypageAPIView,
+    AccountRetrieveAPIView,
+)
 from bodydata.views import BodyDataCreateAPIView, WeightCalculationAPIView
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
@@ -6,6 +10,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("account/mypage/", AccountMypageAPIView.as_view(), name="account_mypage"),
     path("account/create/", AccountCreateAPIView.as_view(), name="account_create"),
     path("account/<pk>/", AccountRetrieveAPIView.as_view(), name="account_retrieve"),
     path("bodydata/create/", BodyDataCreateAPIView.as_view(), name="bodydata_create"),
