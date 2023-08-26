c9ed5d38d756af691cae7b4b063bb6eccb576b31
1844ryudai@gmail.com <1844ryudai>
2023-08-26 09:11:01 +0900

    fix: BodyDataCreate api modification

--- project/bodydata/views.py
+++ project/bodydata/views.py
@@ -1,8 +1,5 @@
-from accounts.models import CustomUser
-from django.shortcuts import get_object_or_404
 from rest_framework import permissions, status, views
 from rest_framework.response import Response
-from rest_framework_simplejwt.tokens import AccessToken
 
 from .serializers import BodyDataSerializer, WeightCalculationSerializer
 
@@ -11,14 +8,9 @@ class BodyDataCreateAPIView(views.APIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, *args, **kwargs):
-        token_str = str(request.META["HTTP_AUTHORIZATION"]).replace("Bearer ", "", 1)
-        access_token = AccessToken(token_str)
-
-        user = get_object_or_404(CustomUser, id=access_token["user_id"])
-
         serializer = BodyDataSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        user.bodydata_set.create(**serializer.validated_data)
+        request.user.bodydata_set.create(**serializer.validated_data)
         return Response(serializer.data, status.HTTP_201_CREATED)
 
 
